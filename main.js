9016d78cef2ae32d5ff36f0a74001ad9bac0608e
TanguyIglesias <tanguyiglesias@gmail.com>
2022-06-27 16:38:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import '..src/css/style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> 
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>  
 `
