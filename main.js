14cbe51cc82731cbdf7e10890db4a538addd719c
st2eve <houbart.steeve@gmail.com>
2022-06-28 20:22:13 +0200

    test

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import "./style.scss"
 
 let character = () => {
   axios.get('https://character-database.becode.xyz/characters').then((response) => {
-  
-    console.log(response)
 
     if (typeof response === "undefined") {
       let error = "<h2 class='error'>personnage inexistant</h2>";
@@ -13,9 +11,8 @@ let character = () => {
      for (let i=0; i<response.data.length;i++ ){
       
       let affichage =
-      `<img class="imageCharacter" id="image[i]" src="data:image/png;base64,${response.data[i].image}">`
+      `<img class="imageCharacter" id="image[${i}]" src="data:image/png;base64,${response.data[i].image}">`
       document.querySelector(".body__container-foot").innerHTML+= affichage
-      console.log(response.data[i])
      }
     
     
@@ -25,15 +22,17 @@ let character = () => {
   })
 }
 
-
-
 character()
+
+
 
 
 const bodyBackgroundRandom = ["/src/images/arene1.jpg","/src/images/arene2.jpg","/src/images/arene3.jpg","/src/images/arene4.jpg","/src/images/arene5.png"]
 let imgRand = bodyBackgroundRandom[Math.floor(Math.random() * bodyBackgroundRandom.length)]
 const selectBody = document.querySelector('body')
 selectBody.setAttribute('style', `background-image: url("${imgRand}");`)
+
+
 
 
 function addCharacter(){
@@ -45,5 +44,4 @@ function addCharacter(){
   document.querySelector(".body__container-foot").appendChild(imgAdd)
 
 }
-
 addCharacter()
