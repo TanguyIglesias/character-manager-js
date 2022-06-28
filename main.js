44cd307ad13952835677c3350d00cefe2462398f
st2eve <houbart.steeve@gmail.com>
2022-06-28 02:11:22 +0200

    add bg img random

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-import '..src/css/style.css'
+/*import '..src/css/style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>  
-`
+`*/
+
+
+const bodyBackgroundRandom = ["/src/images/arene1.jpg","/src/images/arene2.jpg","/src/images/arene3.jpg","/src/images/arene4.jpg","/src/images/arene5.png"]
+let imgRand = bodyBackgroundRandom[Math.floor(Math.random() * bodyBackgroundRandom.length)]
+const selectBody = document.querySelector('body')
+selectBody.setAttribute('style', `background-image: url("${imgRand}");`)
