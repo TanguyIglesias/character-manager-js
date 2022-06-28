658e4ab9642c6c297f2bb4ae1b3e2bd49f970b72
st2eve <houbart.steeve@gmail.com>
2022-06-28 21:42:05 +0200

    test

--- main.js
+++ main.js
@@ -1,46 +1,54 @@
 import "./style.scss"
   
 
-let character = () => {
-  axios.get('https://character-database.becode.xyz/characters').then((response) => {
+  let character = () => {
+    axios.get('https://character-database.becode.xyz/characters').then((response) => {
 
-    if (typeof response === "undefined") {
-      let error = "<h2 class='error'>personnage inexistant</h2>";
-      document.querySelector(".body__container-foot").innerHTML = error;
-    }else{
-     for (let i=0; i<response.data.length;i++ ){
+      if (typeof response === "undefined") {
+        let error = "<h2 class='error'>personnage inexistant</h2>";
+        document.querySelector(".body__container-foot").innerHTML = error;
+      }else{
+      for (let i=0; i<response.data.length;i++ ){
+        
+        let affichage =
+        `<img class="imageCharacter" id="image[${i}]" src="data:image/png;base64,${response.data[i].image}">`
+        document.querySelector(".body__container-foot").innerHTML+= affichage
+      }
+
+      }
       
-      let affichage =
-      `<img class="imageCharacter" id="image[${i}]" src="data:image/png;base64,${response.data[i].image}">`
-      document.querySelector(".body__container-foot").innerHTML+= affichage
-     }
+    })
 
-    }
-    
-  })
+    //addCharacter()
+  }
+
+
+
+function randomBG(){
+  const bodyBackgroundRandom = ["/src/images/arene1.jpg","/src/images/arene2.jpg","/src/images/arene3.jpg","/src/images/arene4.jpg","/src/images/arene5.png"]
+  let imgRand = bodyBackgroundRandom[Math.floor(Math.random() * bodyBackgroundRandom.length)]
+  const selectBody = document.querySelector('body')
+  selectBody.setAttribute('style', `background-image: url("${imgRand}");`)
+
+  character()
 }
 
-character()
-
-
-const bodyBackgroundRandom = ["/src/images/arene1.jpg","/src/images/arene2.jpg","/src/images/arene3.jpg","/src/images/arene4.jpg","/src/images/arene5.png"]
-let imgRand = bodyBackgroundRandom[Math.floor(Math.random() * bodyBackgroundRandom.length)]
-const selectBody = document.querySelector('body')
-selectBody.setAttribute('style', `background-image: url("${imgRand}");`)
-
-
-
-function addCharacter(){
+/*function addCharacter(){
   let imgAdd = document.createElement('img')
   imgAdd.setAttribute('class', 'imageCharacter')
   imgAdd.setAttribute('id', 'createdCharacter')
   imgAdd.setAttribute('src', '/src/images/plus.png')
-
-  document.querySelector(".body__container-foot").appendChild(imgAdd)
   
+  document.querySelector(".body__container-foot").appendChild(imgAdd)
+}*/
+
+function test(){
+  console.log('isoké')
 }
 
-addCharacter()
+randomBG()
 
-document.querySelector('#createdCharacter').addEventListener('click', console.log('isoké'))
+
+document.querySelector('#createdCharacter').addEventListener('click', test);
+
 
