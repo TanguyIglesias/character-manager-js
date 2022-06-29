eab3f88859d8907d367329a1ec94ee7a69d7b0d6
st2eve <houbart.steeve@gmail.com>
2022-06-29 16:59:41 +0200

    suite chara manager

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./style.scss"
 
   let character = () => {
     axios.get('https://character-database.becode.xyz/characters').then((response) => {
-
+    
       if (typeof response === "undefined") {
         let error = "<h2 class='error'>personnage inexistant</h2>";
         document.querySelector(".body__container-foot").innerHTML = error;
@@ -11,10 +11,49 @@ import "./style.scss"
         for (let i=0; i<response.data.length;i++ ){
         
           const createImg = document.createElement('img')
-          createImg.setAttribute('class', 'imageCharacter')
+          createImg.setAttribute('class', `imageCharacter img${i}`)
           createImg.setAttribute('id', `image[${i}]`)
           createImg.setAttribute('src', `data:image/png;base64,${response.data[i].image}`)
           document.querySelector(".body__container-foot").appendChild(createImg)
+
+
+          const createDesc=()=>{
+            const name = document.createElement("h2")
+            const subtitle = document.createElement("h4")
+            const img = document.createElement("div")
+            const description = document.createElement("div")
+            name.innerText = response.data[i].name
+            subtitle.innerText = response.data[i].shortDescription
+            img.innerHTML = `<img src="data:image/png;base64,${response.data[i].image}">`
+            description.innerHTML = response.data[i].description
+
+            if(document.querySelector('.descOne').childNodes.length !== 0){
+              document.querySelector(".descTwo").appendChild(name)
+              document.querySelector(".descTwo").appendChild(subtitle)
+              document.querySelector(".descTwo").appendChild(img)
+              document.querySelector(".descTwo").appendChild(description)
+            }else{
+              
+              document.querySelector(".descOne").appendChild(name)
+              document.querySelector(".descOne").appendChild(subtitle)
+              document.querySelector(".descOne").appendChild(img)
+              document.querySelector(".descOne").appendChild(description)
+            }
+
+            
+          }
+          let selectImgs = document.querySelector(`.img${i}`)
+          //selectImgs.addEventListener("click", createDesc)
+          
+            selectImgs.addEventListener('click', function(){
+              if (document.querySelector('.descTwo').childNodes.length !== 0){
+                console.log('pas de place')
+              }else{
+                createDesc()
+              }
+              });
+
+            
 
         }
 
@@ -34,11 +73,87 @@ function randomBG(){
   character()
 }
 
-function test(){
-  console.log('isoké');
+function createFormAdd(){
+  
+  const createForm = document.createElement('form')
+  createForm.setAttribute('class', 'form')
+  document.querySelector('.form-container').appendChild(createForm)
+
+  const createCharName = document.createElement('input')
+  createCharName.setAttribute('type', 'text')
+  createCharName.setAttribute('placeholder', 'Name')
+  createCharName.setAttribute('class', 'charName')
+
+  const createCharTitle = document.createElement('input')
+  createCharTitle.setAttribute('type', 'text')
+  createCharTitle.setAttribute('placeholder', 'Short Description')
+  createCharTitle.setAttribute('class', 'charTitle')
+
+  const createCharDesc = document.createElement('input')
+  createCharDesc.setAttribute('type', 'text')
+  createCharDesc.setAttribute('placeholder', 'Description')
+  createCharDesc.setAttribute('class', 'charDesc')
+
+  const createCharImg = document.createElement('input')
+  createCharImg.setAttribute('type', 'file')
+  createCharImg.setAttribute('accept', 'image/png, image/jpeg')
+  createCharImg.setAttribute('placeholder', 'Image')
+  createCharImg.setAttribute('class', 'charImg')
+
+  const createSubmit = document.createElement('input')
+  createSubmit.setAttribute('type', 'button')
+  createSubmit.setAttribute('class', 'charSubmit')
+
+  createForm.appendChild(createCharName)
+  createForm.appendChild(createCharTitle)
+  createForm.appendChild(createCharDesc)
+  createForm.appendChild(createCharImg)
+  createForm.appendChild(createSubmit)
+
+  const buttonSubmit = document.querySelector('.charSubmit')
+
+  buttonSubmit.addEventListener('click', getInfos)
+  let chara = {
+    "name" : `${createCharName.value}`,
+    "shortDescription":createCharTitle.innerText,
+    "description":createCharDesc.value,
+    "image": createCharImg.value
+  }
+  return chara
 }
+    
 
 randomBG()
 
 
-document.querySelector('#createdCharacter').addEventListener('click', test);
+document.querySelector('#createdCharacter').addEventListener('click', function(){
+
+  if(document.querySelector('.form')){
+    document.querySelector('.form').remove()
+    createFormAdd()
+  }else{
+    createFormAdd()
+  }
+
+});
+
+
+function test(){
+  console.log('isoké')
+}
+
+function getInfos(){
+
+  let infoName = document.querySelector('.charName').value
+  let infoTitle = document.querySelector('.charTitle').value
+  let infoDesc = document.querySelector('.charDesc').value
+  let infoImg = document.querySelector('.charImg').value
+
+  console.log(infoName)
+  console.log(infoTitle)
+  console.log(infoDesc)
+  // console.log(infoImg)
+  console.log(createFormAdd())
+
+}
+
