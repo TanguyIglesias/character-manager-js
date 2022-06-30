a0ecede5b6fcb41d521cc7f670200b35fa088de0
st2eve <houbart.steeve@gmail.com>
2022-06-30 17:01:35 +0200

    fin fonctionnalités

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ import "./style.scss"
             const subtitle = document.createElement("h4")
             const img = document.createElement("div")
             const description = document.createElement("div")
+            const imgID = response.data[i].id
             name.innerText = response.data[i].name
             subtitle.innerText = response.data[i].shortDescription
             img.innerHTML = `<img src="data:image/png;base64,${response.data[i].image}">`
@@ -33,7 +34,70 @@ import "./style.scss"
               document.querySelector(".descTwo").appendChild(subtitle)
               document.querySelector(".descTwo").appendChild(img)
               document.querySelector(".descTwo").appendChild(description)
+
+              document.querySelector("#deleteCharaTwo").addEventListener("click", function(){
+
+                let url = `https://character-database.becode.xyz/characters/${imgID}`
+                const confirmation = confirm('Are you sur you want to delete this masterpiece ?')
+  
+                if(confirmation){
+                  axios({
+                    method: "delete",
+                    url: url,
+                    data: response.data[i]
+                  })
+                  .then(function (response) {
+                    console.log(response);
+                    location.reload()
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+                }
+              })
+
+              document.querySelector("#editCharaTwo").addEventListener("click", function(){
+                let url = `https://character-database.becode.xyz/characters/${imgID}`
+                if(document.querySelector('.form')){
+                  document.querySelector('.form').remove()
+                  createFormAdd()
+                  document.querySelector('.charSubmit').setAttribute('style', 'display: none;')
+                  document.querySelector('.charSubmitEdit').setAttribute('style', 'display: initial;')
+
+                  let infoName = document.querySelector('.charName')
+                  let infoTitle = document.querySelector('.charTitle')
+                  let infoDesc = document.querySelector('.charDesc')
+                  let infoImg = document.querySelector('#output').src
+    
+                  infoName.value = response.data[i].name
+                  infoTitle.value = response.data[i].shortDescription
+                  infoDesc.value = response.data[i].description
+                  infoImg.file = response.data[i].image
+                }else{
+                  createFormAdd()
+                  document.querySelector('.charSubmit').setAttribute('style', 'display: none;')
+                  document.querySelector('.charSubmitEdit').setAttribute('style', 'display: initial;')
+                 
+                  
+                  let infoName = document.querySelector('.charName')
+                  let infoTitle = document.querySelector('.charTitle')
+                  let infoDesc = document.querySelector('.charDesc')
+                  let infoImg = document.querySelector('#output')
+                  let charaId = response.data[i].id
+
+                  infoName.value = response.data[i].name
+                  infoTitle.value = response.data[i].shortDescription
+                  infoDesc.value = response.data[i].description
+                  infoImg.src = `data:image/png;base64,${response.data[i].image}`
+                  submitEdit(charaId)
+
+                }
+                
+              })
               
+
+
+
 
             }else{
               inputDelEditOne()
@@ -41,10 +105,72 @@ import "./style.scss"
               document.querySelector(".descOne").appendChild(subtitle)
               document.querySelector(".descOne").appendChild(img)
               document.querySelector(".descOne").appendChild(description)
+
+              document.querySelector("#deleteCharaOne").addEventListener("click", function(){
+
+                let url = `https://character-database.becode.xyz/characters/${imgID}`
+                const confirmation = confirm('Are you sur you want to delete this masterpiece ?')
+  
+                if(confirmation){
+                  axios({
+                    method: "delete",
+                    url: url,
+                    data: response.data[i]
+                  })
+                  .then(function (response) {
+                    console.log(response);
+                    location.reload()
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+                }
+              })
+              
+              document.querySelector("#editCharaOne").addEventListener("click", function(){
+                let url = `https://character-database.becode.xyz/characters/${imgID}`
+                if(document.querySelector('.form')){
+                  document.querySelector('.form').remove()
+                  createFormAdd()
+                  document.querySelector('.charSubmit').setAttribute('style', 'display: none;')
+                  document.querySelector('.charSubmitEdit').setAttribute('style', 'display: initial;')
+
+
+                  let infoName = document.querySelector('.charName')
+                  let infoTitle = document.querySelector('.charTitle')
+                  let infoDesc = document.querySelector('.charDesc')
+                  let infoImg = document.querySelector('#output').src
+    
+                  infoName.value = response.data[i].name
+                  infoTitle.value = response.data[i].shortDescription
+                  infoDesc.value = response.data[i].description
+                  infoImg.file = response.data[i].image
+                }else{
+                  createFormAdd()
+                  document.querySelector('.charSubmit').setAttribute('style', 'display: none;')
+                  document.querySelector('.charSubmitEdit').setAttribute('style', 'display: initial;')
+
+                  let infoName = document.querySelector('.charName')
+                  let infoTitle = document.querySelector('.charTitle')
+                  let infoDesc = document.querySelector('.charDesc')
+                  let infoImg = document.querySelector('#output')
+                  
+                  let charaId = response.data[i].id
+                  infoName.value = response.data[i].name
+                  infoTitle.value = response.data[i].shortDescription
+                  infoDesc.value = response.data[i].description
+                  infoImg.src = `data:image/png;base64,${response.data[i].image}`
+                  submitEdit(charaId)
+
+                }
+              })
+              
+
               
             }
-
             
+          
+             
           }
           let selectImgs = document.querySelector(`.img${i}`)
           //selectImgs.addEventListener("click", createDesc)
@@ -106,6 +232,11 @@ function createFormAdd(){
   createSubmit.setAttribute('type', 'button')
   createSubmit.setAttribute('class', 'charSubmit')
 
+  const createSubmitEdit = document.createElement('input')
+  createSubmitEdit.setAttribute('type', 'button')
+  createSubmitEdit.setAttribute('class', 'charSubmitEdit')
+  createSubmitEdit.setAttribute('style', 'display: none;')
+
   const divImg = document.createElement('div')
   const img = document.createElement('img')
   img.setAttribute('id','output')
@@ -115,6 +246,7 @@ function createFormAdd(){
   createForm.appendChild(createCharDesc)
   createForm.appendChild(createCharImg)
   createForm.appendChild(createSubmit)
+  createForm.appendChild(createSubmitEdit)
   createForm.appendChild(divImg)
   divImg.appendChild(img)
 
@@ -206,12 +338,14 @@ function inputDelEditOne(){
   const inputEditOne = document.createElement('input')
   inputEditOne.setAttribute('id', 'editCharaOne')
   inputEditOne.setAttribute('value', 'edit')
+  inputEditOne.setAttribute('type', 'button')
   document.querySelector('.descOne').appendChild(inputEditOne)
 
 
   const inputDeleteOne = document.createElement('input')
   inputDeleteOne.setAttribute('id', 'deleteCharaOne')
   inputDeleteOne.setAttribute('value', 'Delete')
+  inputDeleteOne.setAttribute('type', 'button')
   document.querySelector('.descOne').appendChild(inputDeleteOne)
 }
 
@@ -219,11 +353,60 @@ function inputDelEditTwo(){
   const inputEditTwo = document.createElement('input')
   inputEditTwo.setAttribute('id', 'editCharaTwo')
   inputEditTwo.setAttribute('value', 'edit')
+  inputEditTwo.setAttribute('type', 'button')
   document.querySelector('.descTwo').appendChild(inputEditTwo)
 
 
   const inputDeleteTwo = document.createElement('input')
   inputDeleteTwo.setAttribute('id', 'deleteCharaTwo')
   inputDeleteTwo.setAttribute('value', 'Delete')
+  inputDeleteTwo.setAttribute('type', 'button')
   document.querySelector('.descTwo').appendChild(inputDeleteTwo)
+}
+
+function submitEdit(id){
+  let url ='https://character-database.becode.xyz/characters';
+  fetch(url)
+  .then(response => {
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json();
+  })
+    .then(data => {
+      console.log(data);
+      document.querySelector(".charSubmitEdit").addEventListener("click", function(){
+          let urlId = `https://character-database.becode.xyz/characters/${id}`
+        
+        const confirmation = confirm('Are you sur you want to edit this masterpiece ?')
+          
+          if(confirmation){
+            let infoName = document.querySelector('.charName').value
+            let infoTitle = document.querySelector('.charTitle').value
+            let infoDesc = document.querySelector('.charDesc').value
+            let infoImg = document.querySelector('#output').src
+            let dataImg = infoImg.replace('data:image/png;base64,', '').replace('data:image/jpeg;base64,', '');
+            
+            let character ={
+              description: infoDesc,
+              image: dataImg,
+              name: infoName,
+              shortDescription: infoTitle
+              }
+              axios({
+                method: "put",
+                url: urlId,
+                data: character
+              })
+              .then(function (response) {
+                console.log(response);
+                location.reload()
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+    }
+  })
+})
+.catch(error => {
+  console.log('Noob !', error);
+});
 }
