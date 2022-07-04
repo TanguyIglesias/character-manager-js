e9d9fa5610f21baa4e093f9bdc4b82ca0a46aa68
st2eve <houbart.steeve@gmail.com>
2022-07-04 09:40:34 +0200

    plus de bg

--- main.js
+++ main.js
@@ -237,7 +237,7 @@ import "./style.scss"
 
 
 function randomBG(){
-  const bodyBackgroundRandom = ["/src/images/arene1.jpg","/src/images/arene2.jpg","/src/images/arene3.jpg","/src/images/arene4.jpg","/src/images/arene5.png",'/src/images/arene6.jpg']
+  const bodyBackgroundRandom = ["/src/images/arene1.jpg","/src/images/arene2.jpg","/src/images/arene3.jpg","/src/images/arene4.jpg","/src/images/arene5.png", '/src/images/blackops.png', '/src/images/csgo.png', '/src/images/fantasy.jpg', '/src/images/halo.png', '/src/images/lol.png', '/src/images/minecraft.png', '/src/images/smash.png', '/src/images/smite.jpg', '/src/images/tft.png']
   let imgRand = bodyBackgroundRandom[Math.floor(Math.random() * bodyBackgroundRandom.length)]
   const selectBody = document.querySelector('body')
   selectBody.setAttribute('style', `background-image: url("${imgRand}");`)
